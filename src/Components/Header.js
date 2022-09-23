77084bd722348dc3328621720e440170683c8e9b
sudharson-31 <96968997+sudharson-31@users.noreply.github.com>
2022-09-23 18:58:30 +0530

    Updates

--- src/Components/Header.js
+++
@@ -1,9 +0,0 @@
-export default function Header(){
-    return(
-        <div id="Header">
-            <span id="logo-container">
-                <img src={process.env.PUBLIC_URL+'/images/logo.svg'} alt="logo"></img>
-            </span>
-        </div>
-    )
-}
